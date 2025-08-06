bcc7666d9abebc14ebec4df21c7917639d057201
Elijah Bafford <ew.bafford@gmail.com>
2025-08-06 15:31:24 -0400

    Improved Level Main prefab

--- Assets/Data/Scripts/Level/DeathPlane.cs
+++ Assets/Data/Scripts/Level/DeathPlane.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class DeathPlane : MonoBehaviour {
 
-    [Header("The height at which the player dies from falling off the map")]
-    [SerializeField] private float yPosition;
+    [Header("The Y position of this object determines how far the player\ncan fall before dying")]
+    [SerializeField] private float thisVariableIsReadOnly;
     private Transform playerTransform;
     private Player player;
     private bool isDead = false;
 
     private void Start() {
         isDead = false;
+        thisVariableIsReadOnly = transform.position.y;
         player = Player.player;
         playerTransform = player.GetTransform();
     }
 
     private void FixedUpdate() {
         if (isDead) return;
-        if (playerTransform.position.y <= yPosition) {
+        if (playerTransform.position.y <= thisVariableIsReadOnly) {
             player.Die();
         }
     }
